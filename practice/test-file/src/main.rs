a681d8461f8099c367149315367480a2b50663fb
Jiaming Lu <jiaming.lu.0124@gamil.com>
2023-05-23 16:33:09 +0800

    20230522

--- practice/test-file/src/main.rs
+++ practice/test-file/src/main.rs
@@ -1,9 +1,6 @@
+// Fill the blank
 fn main() {
-    let (x, y);
-    (x,..) = (3, 4);
-    [.., y] = [1, 2];
-    // Fill the blank to make the code work
-    assert_eq!([x,y], [3, 2]);
+    let v: u16 = 38_u8 as u16;
 
     println!("Success!");
-} 
+}
